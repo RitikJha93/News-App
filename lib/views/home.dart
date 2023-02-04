fc8d8511b7ccd7ea198277d2e705e5d26bf02fb1
ritik jha <coolritikjha46@gmail.com>
2023-02-04 15:46:26 +0530

    inital commit

---
+++ lib/views/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Flutter', style: TextStyle(color: Colors.black)),
+            Text(
+              'News',
+              style: TextStyle(color: Colors.blue[300]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
